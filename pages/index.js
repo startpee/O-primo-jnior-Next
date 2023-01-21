a95a08f4c46745c47d20345f7c429d959bdf62ee
Startpee <86379169+startpee@users.noreply.github.com>
2023-01-21 20:06:16 -0300

    Commit from TeleportHQ

--- pages/index.js
+++ pages/index.js
@@ -253,6 +253,8 @@ const Home = (props) => {
           .home-div {
             width: 100%;
             height: 100%;
+            margin-top: 30px;
+            margin-bottom: 30px;
           }
           .home-section2 {
             flex: 0 0 auto;
