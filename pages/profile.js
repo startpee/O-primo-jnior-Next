279dbbf3c9cda2ae121ba2a83d52ed8f897770ee
Startpee <86379169+startpee@users.noreply.github.com>
2023-03-23 20:55:51 -0300

    Commit from TeleportHQ

--- pages/profile.js
+++ pages/profile.js
@@ -77,34 +77,24 @@ const Profile = (props) => {
               <svg viewBox="0 0 1024 1024" className="profile-icon">
                 <path d="M512 490q44 0 75-31t31-75-31-75-75-31-75 31-31 75 31 75 75 31zM512 86q124 0 211 87t87 211q0 62-31 142t-75 150-87 131-73 97l-32 34q-12-14-32-37t-72-92-91-134-71-147-32-144q0-124 87-211t211-87z"></path>
               </svg>
-              <span className="profile-text01 TextSM">São Paulo - SP</span>
+              <span className="profile-text1 TextSM">São Paulo - SP</span>
             </div>
             <div className="profile-container08">
               <svg viewBox="0 0 1024 1024" className="profile-icon2">
                 <path d="M810.667 213.333h-597.333l64-85.333h469.333zM929.877 230.059l-127.744-170.325c-8.363-11.136-21.077-17.024-34.133-17.067h-512c-13.909 0-26.283 6.656-34.133 17.067l-127.744 170.325c-1.835 2.389-3.456 5.035-4.736 7.808-2.773 5.803-4.096 12.032-4.053 18.133v597.333c0 35.328 14.379 67.413 37.504 90.496s55.168 37.504 90.496 37.504h597.333c35.328 0 67.413-14.379 90.496-37.504s37.504-55.168 37.504-90.496v-597.333c0-9.344-3.029-18.005-8.064-24.96-0.171-0.213-0.299-0.427-0.469-0.64zM170.667 298.667h682.667v554.667c0 11.776-4.736 22.4-12.501 30.165s-18.389 12.501-30.165 12.501h-597.333c-11.776 0-22.4-4.736-30.165-12.501s-12.501-18.389-12.501-30.165zM640 426.667c0 35.371-14.293 67.285-37.504 90.496s-55.125 37.504-90.496 37.504-67.285-14.293-90.496-37.504-37.504-55.125-37.504-90.496c0-23.552-19.115-42.667-42.667-42.667s-42.667 19.115-42.667 42.667c0 58.88 23.936 112.299 62.464 150.869s91.989 62.464 150.869 62.464 112.299-23.936 150.869-62.464 62.464-91.989 62.464-150.869c0-23.552-19.115-42.667-42.667-42.667s-42.667 19.115-42.667 42.667z"></path>
               </svg>
-              <span className="profile-text02">empreendedor</span>
+              <span className="profile-text2">empreendedor</span>
             </div>
             <div className="profile-container09"></div>
-            <span className="profile-text03">
-              <span className="profile-text04">
+            <span className="profile-text3">
+              <span className="profile-text4">
                 Nascido em 1994 em jaru Rondônia
               </span>
               <br></br>
-              <span>
-                já morei em alguns estados desse brasil
-                :Piauí,Maranhão,Amazonas,Ronodônia,Mato grosso
-              </span>
-              <br></br>
-              <span>
-                Meu pai era quase um Nômade não parava quieto por isso me mudei
-                bastante.
-              </span>
-              <br></br>
-              <br></br>
+              <span>Piauí,Amazonas,Maranhão,Rondõnia já </span>
               <br></br>
             </span>
-            <span className="profile-text12">Ler mais</span>
+            <span className="profile-text8">Ler mais</span>
           </div>
         </div>
         <Footer></Footer>
@@ -245,7 +235,7 @@ const Profile = (props) => {
             width: 18px;
             height: 18px;
           }
-          .profile-text01 {
+          .profile-text1 {
             color: var(--dl-color-secondary-500);
             font-weight: 600;
             margin-left: var(--dl-space-space-halfunit);
@@ -263,7 +253,7 @@ const Profile = (props) => {
             width: 18px;
             height: 18px;
           }
-          .profile-text02 {
+          .profile-text2 {
             color: var(--dl-color-secondary-500);
             margin-left: var(--dl-space-space-halfunit);
           }
@@ -279,7 +269,7 @@ const Profile = (props) => {
             flex-direction: column;
             background-color: var(--dl-color-gray-700);
           }
-          .profile-text03 {
+          .profile-text3 {
             width: 75%;
             font-size: 1.125rem;
             text-align: center;
@@ -287,18 +277,18 @@ const Profile = (props) => {
             line-height: 1.625;
             margin-bottom: var(--dl-space-space-unit);
           }
-          .profile-text04 {
+          .profile-text4 {
             width: 75%;
             text-align: center;
             margin-bottom: var(--dl-space-space-unit);
           }
-          .profile-text12 {
+          .profile-text8 {
             color: var(--dl-color-pimary-500);
             font-style: normal;
             transition: 0.3s;
             font-weight: 500;
           }
-          .profile-text12:hover {
+          .profile-text8:hover {
             color: var(--dl-color-pimary-300);
           }
           @media (max-width: 991px) {
@@ -311,16 +301,16 @@ const Profile = (props) => {
             }
           }
           @media (max-width: 479px) {
-            .profile-text02 {
+            .profile-text2 {
               color: var(--dl-color-secondary-500);
             }
-            .profile-text03 {
+            .profile-text3 {
               color: var(--dl-color-secondary-300);
               font-size: 1.125rem;
               font-weight: 300;
               line-height: 1.625;
             }
-            .profile-text12 {
+            .profile-text8 {
               color: var(--dl-color-pimary-500);
               font-style: normal;
               font-weight: 500;
