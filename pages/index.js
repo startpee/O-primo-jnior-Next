0764a6799fdb7c0ec13048cbbe482e833eb53f62
Startpee <86379169+startpee@users.noreply.github.com>
2023-04-05 06:48:44 -0300

    Commit from TeleportHQ

--- pages/index.js
+++ pages/index.js
@@ -77,8 +77,32 @@ const Home = (props) => {
             </div>
           </div>
           <div className="home-banner">
-            <h1 className="home-text08">Quer fazer parte do nosso projeto?</h1>
-            <span className="home-text09">
+            <h1 className="home-text08">
+              <span>
+                Assista esse
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span className="home-text10">video</span>
+            </h1>
+            <video
+              src
+              poster="https://play.teleporthq.io/static/svg/videoposter.svg"
+              id="H7XWraOl4-g"
+              className="home-video"
+            ></video>
+          </div>
+          <div className="home-banner1">
+            <video
+              src
+              poster="https://play.teleporthq.io/static/svg/videoposter.svg"
+              className="home-video1"
+            ></video>
+            <h1 className="home-text11">Quer fazer parte do nosso projeto?</h1>
+            <span className="home-text12">
               juntos nós tornamos forte e crescemos 
             </span>
             <div className="home-btn-group">
@@ -95,12 +119,12 @@ const Home = (props) => {
         </div>
         <div className="home-section3">
           <div className="home-container05">
-            <h6 className="home-text10 TextSM">
+            <h6 className="home-text13 TextSM">
               <span>ON THE COASTLINE</span>
             </h6>
-            <h3 className="home-text12 Healine">We are missing the summer</h3>
-            <span className="home-text13 TextXL">
-              <span className="home-text14">
+            <h3 className="home-text15 Healine">We are missing the summer</h3>
+            <span className="home-text16 TextXL">
+              <span className="home-text17">
                 We&apos;re waiting for the weather to warm up so that we can go
                 to the coastline. We really miss the summertime, our second home
                 the coastline.
@@ -115,8 +139,8 @@ const Home = (props) => {
                     <path d="M832 192v-128h-640v128h-192v128c0 106.038 85.958 192 192 192 20.076 0 39.43-3.086 57.62-8.802 46.174 66.008 116.608 113.796 198.38 130.396v198.406h-64c-70.694 0-128 57.306-128 128h512c0-70.694-57.306-128-128-128h-64v-198.406c81.772-16.6 152.206-64.386 198.38-130.396 18.19 5.716 37.544 8.802 57.62 8.802 106.042 0 192-85.962 192-192v-128h-192zM192 436c-63.962 0-116-52.038-116-116v-64h116v64c0 40.186 7.43 78.632 20.954 114.068-6.802 1.246-13.798 1.932-20.954 1.932zM948 320c0 63.962-52.038 116-116 116-7.156 0-14.152-0.686-20.954-1.932 13.524-35.436 20.954-73.882 20.954-114.068v-64h116v64z"></path>
                   </svg>
                 </div>
-                <h1 className="home-text15 TextXL">Grow your market</h1>
-                <span className="home-text16">
+                <h1 className="home-text18 TextXL">Grow your market</h1>
+                <span className="home-text19">
                   Some quick example text to build on the card title and make up
                   the bulk of the card&apos;s content.
                 </span>
@@ -127,8 +151,8 @@ const Home = (props) => {
                     <path d="M726 726v-172h-86v172h86zM554 726v-428h-84v428h84zM384 726v-300h-86v300h86zM810 128q34 0 60 26t26 60v596q0 34-26 60t-60 26h-596q-34 0-60-26t-26-60v-596q0-34 26-60t60-26h596z"></path>
                   </svg>
                 </div>
-                <h1 className="home-text17 TextXL">Launch time</h1>
-                <span className="home-text18">
+                <h1 className="home-text20 TextXL">Launch time</h1>
+                <span className="home-text21">
                   Some quick example text to build on the card title and make up
                   the bulk of the card&apos;s content.
                 </span>
@@ -136,31 +160,31 @@ const Home = (props) => {
             </div>
             <div className="home-contact">
               <Label text="MESSAGE US"></Label>
-              <h2 className="home-text19">Type in the bellow form</h2>
-              <span className="home-text20 TextXL">
+              <h2 className="home-text22">Type in the bellow form</h2>
+              <span className="home-text23 TextXL">
                 They say there&apos;s no future for the street rascals, nothing
                 for them, leave them to fall. But it isn&apos;t always how you
                 would like it to be, especially when you do nothing for
                 yourself.
               </span>
               <div className="home-form">
-                <h1 className="home-text21">Want to work with us?</h1>
-                <span className="home-text22 TextXL">
+                <h1 className="home-text24">Want to work with us?</h1>
+                <span className="home-text25 TextXL">
                   Complete this form and we will get back to you in 24 hours.
                 </span>
-                <span className="home-text23 TextXS">FULL NAME</span>
+                <span className="home-text26 TextXS">FULL NAME</span>
                 <input
                   type="text"
                   placeholder="Full Name"
                   className="home-textinput TextSM input"
                 />
-                <span className="home-text24 TextXS">EMAIL</span>
+                <span className="home-text27 TextXS">EMAIL</span>
                 <input
                   type="text"
                   placeholder="Email"
                   className="home-textinput1 TextSM input"
                 />
-                <span className="home-text25 TextXS">MESSAGE</span>
+                <span className="home-text28 TextXS">MESSAGE</span>
                 <textarea
                   cols="80"
                   rows="4"
@@ -329,18 +353,44 @@ const Home = (props) => {
           }
           .home-banner {
             width: 100%;
+            height: 523px;
+            display: flex;
+            padding: 48px;
+            align-items: center;
+            margin-bottom: 103px;
+            flex-direction: column;
+            justify-content: space-between;
+          }
+          .home-text08 {
+            color: rgb(108, 119, 251);
+            font-size: 3rem;
+            text-align: center;
+          }
+          .home-text10 {
+            color: #000000;
+          }
+          .home-video {
+            width: 660px;
+            height: 452px;
+          }
+          .home-banner1 {
+            width: 100%;
             display: flex;
             padding: 48px;
             align-items: center;
             flex-direction: column;
             justify-content: space-between;
           }
-          .home-text08 {
+          .home-video1 {
+            width: 320px;
+            height: 180px;
+          }
+          .home-text11 {
             color: #6c77fb;
             font-size: 3rem;
             text-align: center;
           }
-          .home-text09 {
+          .home-text12 {
             max-width: 1400px;
             margin-top: 32px;
             text-align: center;
@@ -407,20 +457,20 @@ const Home = (props) => {
             padding-right: var(--dl-space-space-unit);
             flex-direction: column;
           }
-          .home-text10 {
+          .home-text13 {
             font-style: normal;
             font-weight: 700;
           }
-          .home-text12 {
+          .home-text15 {
             color: var(--dl-color-gray-white);
           }
-          .home-text13 {
+          .home-text16 {
             width: 80%;
             text-align: center;
             font-weight: 300;
             margin-bottom: 0.25rem;
           }
-          .home-text14 {
+          .home-text17 {
             white-space: normal;
           }
           .home-services {
@@ -457,12 +507,12 @@ const Home = (props) => {
             width: 24px;
             height: 24px;
           }
-          .home-text15 {
+          .home-text18 {
             color: var(--dl-color-gray-white);
             margin-top: var(--dl-space-space-unitandhalf);
             font-weight: 600;
           }
-          .home-text16 {
+          .home-text19 {
             color: rgba(100, 116, 139, 1);
             font-size: 1.25rem;
             text-align: center;
@@ -491,12 +541,12 @@ const Home = (props) => {
             width: 24px;
             height: 24px;
           }
-          .home-text17 {
+          .home-text20 {
             color: var(--dl-color-gray-white);
             margin-top: var(--dl-space-space-unitandhalf);
             font-weight: 600;
           }
-          .home-text18 {
+          .home-text21 {
             color: rgba(100, 116, 139, 1);
             font-size: 1.25rem;
             text-align: center;
@@ -508,11 +558,11 @@ const Home = (props) => {
             margin-bottom: var(--dl-space-space-doubleunit);
             flex-direction: column;
           }
-          .home-text19 {
+          .home-text22 {
             color: var(--dl-color-gray-white);
             margin-top: var(--dl-space-space-halfunit);
           }
-          .home-text20 {
+          .home-text23 {
             color: rgba(255, 255, 255, 0.75);
             width: 80%;
             text-align: center;
@@ -530,16 +580,16 @@ const Home = (props) => {
             flex-direction: column;
             background-color: var(--dl-color-pimary-800);
           }
-          .home-text21 {
+          .home-text24 {
             color: var(--dl-color-secondary-400);
             font-size: 1.5rem;
             font-weight: 600;
             line-height: 2rem;
           }
-          .home-text22 {
+          .home-text25 {
             color: rgba(100, 116, 139, 1);
           }
-          .home-text23 {
+          .home-text26 {
             color: rgba(100, 116, 139, 1);
             margin-top: var(--dl-space-space-halfunit);
             margin-left: 0.25rem;
@@ -560,7 +610,7 @@ const Home = (props) => {
             color: var(--dl-color-secondary-500);
             box-shadow: 0 0 0 2px#4AA4E3;
           }
-          .home-text24 {
+          .home-text27 {
             color: rgba(100, 116, 139, 1);
             margin-top: var(--dl-space-space-halfunit);
             margin-left: 0.25rem;
@@ -581,7 +631,7 @@ const Home = (props) => {
             color: var(--dl-color-secondary-500);
             box-shadow: 0 0 0 2px#4AA4E3;
           }
-          .home-text25 {
+          .home-text28 {
             color: rgba(100, 116, 139, 1);
             margin-top: var(--dl-space-space-halfunit);
             margin-left: 0.25rem;
@@ -690,7 +740,11 @@ const Home = (props) => {
               padding-left: 32px;
               padding-right: 32px;
             }
-            .home-text09 {
+            .home-banner1 {
+              padding-left: 32px;
+              padding-right: 32px;
+            }
+            .home-text12 {
               padding-left: var(--dl-space-space-unit);
               padding-right: var(--dl-space-space-unit);
             }
@@ -727,6 +781,19 @@ const Home = (props) => {
             .home-text08 {
               font-size: 2rem;
             }
+            .home-video {
+              width: 409px;
+              height: 353px;
+            }
+            .home-banner1 {
+              padding-top: 32px;
+              padding-left: var(--dl-space-space-unit);
+              padding-right: var(--dl-space-space-unit);
+              padding-bottom: 32px;
+            }
+            .home-text11 {
+              font-size: 2rem;
+            }
             .home-btn-group {
               flex-direction: column;
             }
@@ -734,13 +801,13 @@ const Home = (props) => {
               margin-top: var(--dl-space-space-unit);
               margin-left: 0px;
             }
-            .home-text12 {
+            .home-text15 {
               text-align: center;
             }
-            .home-text19 {
+            .home-text22 {
               text-align: center;
             }
-            .home-text21 {
+            .home-text24 {
               text-align: left;
             }
           }
